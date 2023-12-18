af3ef6de309056e83cb7a02d21009314dc3fcd3c
Elchedli <shidonosan@gmail.com>
2023-12-18 17:26:37 +0100

    progress: 6-Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\studentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+
+//Routing part
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::get('/students', function () {
+    return view('students');
+});
+
+Route::get('/student', function () {
+    return response()->json([
+        'name' => 'name1',
+        'lastname' => 'lastname1',
+    ]);
+});
+
+Route::get('/student1', function () {
+    return redirect('/welcome');
+});
+
+//Controller part
+
+//Method 1
+// Route::get('/student',[studentController::class,'Student']);
+
+//Method 2
+Route::get('student','App\Http\Controllers\studentController@Student');
