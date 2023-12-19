961dc5f8b5cb1f2073645c2cd32a2d6718d9ac57
Elchedli <shidonosan@gmail.com>
2023-12-19 16:51:39 +0100

    fix: changes that made seeding command work

--- database/migrations/2023_12_19_135712_create_customers_table.php
+++ database/migrations/2023_12_19_135712_create_customers_table.php
@@ -21,7 +21,7 @@ class CreateCustomersTable extends Migration
             $table->string('address'); 
             $table->string('city'); 
             $table->string('state'); 
-            $table->string('postal_code '); 
+            $table->string('postal_code'); 
             $table->timestamps();
         });
     }
