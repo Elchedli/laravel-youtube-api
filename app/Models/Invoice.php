c0598aac03ebf9509fb046f1bff5c2918453e73e
Elchedli <shidonosan@gmail.com>
2023-12-19 15:25:24 +0100

    feat: added fake data with customer and invoices

---
+++ app/Models/Invoice.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Invoice extends Model
+{
+    use HasFactory;
+
+    public function customer(){
+        return $this->belongsTo(Customer::class);
+    }
+}
---
+++ database/factories/InvoiceFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Customer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class InvoiceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $status = $this->faker->randomElement(['B','P','V']);
+        return [
+            'customer_id' => Customer::factory(),
+            'amount' => $this->faker->numberBetween(100,20000),
+            'status' => $status,
+            'billed_data' => $this->faker->dateTimeThisDecade(),
+            'paid_date' => $status == 'P' ? $this->faker->dateTimeThisDecade() : NULL
+        ];
+    }
+}
