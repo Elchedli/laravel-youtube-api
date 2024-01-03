56ad3e7eba34460ef836b7edc100337d7083a408
elchedli <shidonosan@gmail.com>
2024-01-03 12:14:22 +0100

    feat: youtube DATA V3 filtered correctly with fastest output 13s in Skoda 500+ videos

--- app/Http/Controllers/YoutubeController.php
+++ app/Http/Controllers/YoutubeController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Http;
-use App\Http\Services\Utils\Utilities;
-use stdClass;
 
 class YoutubeController extends Controller
 {
@@ -17,62 +15,52 @@ class YoutubeController extends Controller
         $this->youtubeEndPoint = config('services.youtube.endpoint');
     }
 
-    public function testphp(): bool
+    public function testphp()
     {
         // Jeb2PJ2tvXc => short | o3PqMxYIDH4 => normal video
-        $id = 'o3PqMxYIDH4';
-        // $response = Http::head("https://www.youtube.com/shorts/$id");
-        // info("data : ".$this->isShortVideo($id));
-        return !(Utilities::isRedirection("https://www.youtube.com/shorts/$id"));
     }
 
     //This controller will help us get all videos/shorts/recorder live videos in a channel
     public function profile()
     {
-        //foot channel id : UCvqUz4adCLEpGyQjWXvcK_w
+        //foot africa channel id : UCvqUz4adCLEpGyQjWXvcK_w
         //my channel shidono id : UCj6cRIsQnyrWfwJX2x7S0lg
         //Skoda channel  id : UCjG24cC7xIEkVtxKdhHDwtg
         $idChannel = 'UCjG24cC7xIEkVtxKdhHDwtg';
-
         $channelData = $this->getChannelData($idChannel);
-        $playlistId = $channelData->items[0]->contentDetails->relatedPlaylists->uploads;
-        $channelVideosPartialData = $this->getAllVideos($playlistId);
-        $channelVideos = $this->getVideosContent($channelVideosPartialData);
-        return (object)['channel' => $channelData, 'videos' => $this->filterVideosByType($channelVideos)];
+        $channelID = $channelData->items[0]->id; // this is needed if there is no id
+        return (object) ['channel' => $channelData, 'videos' => $this->getAllVideosFiltered($channelID)];
     }
 
-    // this function get the basic information of a channel,the most important parts are statistics(viewCount,SubscriberCounts...)
+
+    /** This part is Youtube Data API V3 */
+
+     // this function get the basic information of a channel,the most important parts are statistics(viewCount,SubscriberCounts...)
     // In contentDetails that we added from $part we gonna get uploads id which give a playlist containing all videos/shorts/recorded lives in the channel
-    private function getChannelData($idChannel)
+    protected function getChannelData($idChannel)
     {
-        $part = 'snippet,id,statistics,contentDetails';
+        $part = 'snippet,id,statistics';
         $url = "$this->youtubeEndPoint/channels?part=$part&id=$idChannel&key=$this->apiKey";
         return json_decode(Http::get($url));
     }
 
-    // We have a limit of 10.000 quotas per day
-    // Using the playlistId that we got from getChannelData object we use youtube API playlist function because it uses the cheapest searching cost (1 quota)
-    // Since we have 50 max results maximum per request we use a parameter in the url called nextPageToken, we can a portion of the data each request until we fully get all of them
-    private function getAllVideos($playlistId)
+    //** Those are looping functions that uses a lot of quotas, there are consecutive in order */
+
+
+    protected function getAllVideosFiltered($channelID)
     {
-        $part = 'snippet,id';
-        $maxResults = 50;
+        
+        $types = ['videos','shorts','live'];
+        //Array_combine help so it can replicate the value as key instead of 0,1 as array values so array_map becomes easy
+        $allVideosFiltered = array_combine($types, array_map(fn ($type) => $this->getVideoPlaylistByType($type,$channelID),$types));
 
-        $allVideos = [];
-        $nextPageToken = '';
-
-        while (isset($nextPageToken)) {
-            $url = "$this->youtubeEndPoint/playlistItems?part=$part&playlistId=$playlistId&pageToken=$nextPageToken&maxResults=$maxResults&key=$this->apiKey";
-            $body = json_decode(Http::get($url));
-            $nextPageToken = optional($body)->nextPageToken;
-            $allVideos = array_merge($allVideos, $body->items);
-        }
-
-        $allVideosIds = array_map(fn ($singleVideo) => $singleVideo->snippet->resourceId->videoId, $allVideos);
-        return $allVideosIds;
+        //change the array to object
+        return $allVideosFiltered;
     }
+    
 
-    // we get a table of video ID's with getAllVideos that we gonna use to filter category of videos (shorts,videos,live records) and gettings statistics for each video (comments,likes,views...)
+
+    // we get a table of video ID's with getAllVideos that contains more informations about each video like (comments,likes,views...)
     private function getVideosContent($tableVideosIDs)
     {
         $part = 'snippet,contentDetails,statistics,player,liveStreamingDetails';
@@ -87,37 +75,32 @@ class YoutubeController extends Controller
         return $allVideos;
     }
 
-    private function filterVideosByType($Videos)
+     // We have a limit of 10.000 quotas per day
+    // Using the channelID that we got from getChannelData object we use youtube API playlist function because it uses the cheapest searching cost (1 quota)
+    // Since we have 50 max results maximum per request we use a parameter in the url called nextPageToken, we can a portion of the data each request until we fully get all of them
+    private function getVideoPlaylistByType($type, $channelID)
     {
-        
-        $Videos = array_filter($Videos, fn ($singleVideo) => $singleVideo->snippet->liveBroadcastContent !== "upcoming");
-        // Filter conditions
-        $conditions = [
-            'lives' => function ($item) {
-                return isset($item->liveStreamingDetails);
-            },
-            'shorts' => function ($item) {
-                return !(Utilities::isRedirection("https://www.youtube.com/shorts/" . $item->id));
-            },
-        ];
-    
-        // Initialize $tableVideos as an object
-        $tableVideos = new \stdClass();
-        
-        // Apply filters sequentially
-        foreach ($conditions as $key => $condition) {
-            $filteredData = array_filter($Videos, $condition);
-            $tableVideos->$key = $filteredData; // Assign property dynamically
-            $Videos = array_diff_key($Videos, $filteredData);
-        }
-        // dd($filteredData,$Videos,$tableVideos);
-        $tableVideos->normalVideos = $Videos;
-        // dd($filteredData,$Videos,$tableVideos);
-        return $tableVideos;
-        // make a map or filter that divides into three variables [liveStreams,Shorts,Normal videos]
-        // if liveStreamingDetails exist in videos that means Api exist
-    }
+        $part = 'snippet,id';
+        $maxResults = 50;
 
-    
-    
+        $allVideos = [];
+        $nextPageToken = '';
+
+        $channelID = match($type){
+            'shorts' => str_replace('UC','UUSH',$channelID),
+            'videos' => str_replace('UC','UULF',$channelID),
+            'live' => str_replace('UC','UULV',$channelID)
+        };
+
+        while (isset($nextPageToken)) {
+            $url = "$this->youtubeEndPoint/playlistItems?part=$part&playlistId=$channelID&pageToken=$nextPageToken&maxResults=$maxResults&key=$this->apiKey";
+            $body = json_decode(Http::get($url));
+            $nextPageToken = optional($body)->nextPageToken;
+            $allVideos = array_merge($allVideos, $body->items ?? []);
+        }
+
+        $allVideosIDs = array_map(fn($singleVideo) => $singleVideo->snippet->resourceId->videoId, $allVideos);
+
+        return !empty($allVideosIDs) ? $this->getVideosContent($allVideosIDs) : null;
+    }
 }
